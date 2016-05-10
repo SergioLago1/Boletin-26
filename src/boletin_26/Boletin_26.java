969203b405b46bdd6f17fd061fe102fca5a9acc1
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-05-10 09:14:44 +0200

    Fin

---
+++ src/boletin_26/Boletin_26.java
@@ -0,0 +1,10 @@
+package boletin_26;
+public class Boletin_26 {
+    
+    public static void main(String[] args) {
+         Bonoloto bono= new Bonoloto(); 
+         
+         bono.setVisible(true);
+    }
+      
+}
